1175beff0a44b84ef13f132e51ece801e7751071
ねむい <69592455+kok3shidoll@users.noreply.github.com>
2023-05-28 07:27:28 +0900

    update offsets.c: 12H143

--- offsets.c
+++ offsets.c
@@ -13,6 +13,7 @@ static addr_t vtab = 0;
 
 addr_t* offsets = NULL;
 
+/* 8.4.1 */
 static addr_t koffsets_S5L895xX_12H321[] =
 {
     0x802da865, // OSSerializer::serialize
@@ -39,7 +40,7 @@ static addr_t koffsets_S5L895xX_12H321[] =
     0x00000010, // KSTRUCT_OFFSET_PIPE_BUFFER
 };
 static addr_t koffsets_S5L894xX_12H321[] =
-{
+{   // untested
     0x802d5a1d, // OSSerializer::serialize
     0x802d7afd, // OSSymbol::getMetaClass
     0x8001e0a1, // calend_gettime
@@ -55,6 +56,58 @@ static addr_t koffsets_S5L894xX_12H321[] =
     0x80398d08, // vtab
     0x00000044, // IPC_PORT_IP_KOBJECT
     0x000000a4, // task->itk_self
+    0x000001a8, // task->itk_space
+    0x000001f0, // TASK_BSD_INFO
+    0x00000090, // p->p_fd
+    0x00000000, // FILEDESC_FD_OFILES
+    0x00000008, // FILEPROC_F_FGLOB
+    0x00000028, // FILEGLOB_FG_DATA
+    0x00000010, // KSTRUCT_OFFSET_PIPE_BUFFER
+};
+
+/* 8.4 (untested, by offsetfinder) */
+static addr_t koffsets_S5L895xX_12H143[] =
+{   // n42
+    0x802da759, // OSSerializer_serialize
+    0x802dc879, // OSSymbol_getMetaClass
+    0x8001e301, // calend_gettime
+    0x800c75f5, // bufattr_cpx
+    0x803b2cdc, // clock_ops
+    0x800b486c, // copyin
+    0x800c75f6, // bx_lr
+    0x803f9258, // vm_kernel_addrperm
+    0x8003a3f1, // current_task
+    0x80016275, // ipc_port_make_send
+    0x800162fd, // ipc_port_copyout_send
+    0x8040a098, // kernel_task
+    0x8039dd08, // vtab
+    0x00000044, // ip_kobject
+    0x000000a4, // itk_self
+    0x000001a8, // task->itk_space
+    0x000001f0, // TASK_BSD_INFO
+    0x00000090, // p->p_fd
+    0x00000000, // FILEDESC_FD_OFILES
+    0x00000008, // FILEPROC_F_FGLOB
+    0x00000028, // FILEGLOB_FG_DATA
+    0x00000010, // KSTRUCT_OFFSET_PIPE_BUFFER
+};
+static addr_t koffsets_S5L894xX_12H143[] =
+{   // n94
+    0x802d59b9, // OSSerializer_serialize
+    0x802d7a99, // OSSymbol_getMetaClass
+    0x8001e0a1, // calend_gettime
+    0x800c46f9, // bufattr_cpx
+    0x803adcdc, // clock_ops
+    0x800b2724, // copyin
+    0x800c46fa, // bx_lr
+    0x803f4128, // vm_kernel_addrperm
+    0x80039f2d, // current_task
+    0x80016079, // ipc_port_make_send
+    0x80016101, // ipc_port_copyout_send
+    0x80405098, // kernel_task
+    0x80398d08, // vtab
+    0x00000044, // ip_kobject
+    0x000000a4, // itk_self
     0x000001a8, // task->itk_space
     0x000001f0, // TASK_BSD_INFO
     0x00000090, // p->p_fd
