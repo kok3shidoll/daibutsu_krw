d800ccc8a3ac5aba49212269df16455a2cb71783
ねむい <69592455+kok3shidoll@users.noreply.github.com>
2023-05-28 07:20:19 +0900

    update offsets.c: 8940_12H321

--- offsets.c
+++ offsets.c
@@ -15,53 +15,53 @@ addr_t* offsets = NULL;
 
 static addr_t koffsets_S5L895xX_12H321[] =
 {
-    0x802DA865, // OSSerializer::serialize
-    0x802DC985, // OSSymbol::getMetaClass
-    0x8001E301, // calend_gettime
-    0x800C75F5, // _bufattr_cpx
-    0x803B2CDC, // clock_ops
-    0x800B486C, // _copyin
-    0x800C75F6, // BX LR
-    0x803F9258, // vm_kernel_addrperm
+    0x802da865, // OSSerializer::serialize
+    0x802dc985, // OSSymbol::getMetaClass
+    0x8001e301, // calend_gettime
+    0x800c75f5, // _bufattr_cpx
+    0x803b2cdc, // clock_ops
+    0x800b486c, // _copyin
+    0x800c75f6, // BX LR
+    0x803f9258, // vm_kernel_addrperm
     0x8003a3f1, // current_task
     0x80016275, // ipc_port_make_send
     0x800162fd, // ipc_port_copyout_send
     0x8040a098, // kernel_task
-    0x8039DD08, // vtab
-    0x44,       // IPC_PORT_IP_KOBJECT
-    0xa4,       // task->itk_self
-    0x1a8,      // task->itk_space
-    0x1f0,      // TASK_BSD_INFO
-    0x90,       // p->p_fd
-    0x0,        // FILEDESC_FD_OFILES
-    0x8,        // FILEPROC_F_FGLOB
-    0x28,       // FILEGLOB_FG_DATA
-    0x10,       // KSTRUCT_OFFSET_PIPE_BUFFER
+    0x8039dd08, // vtab
+    0x00000044, // IPC_PORT_IP_KOBJECT
+    0x000000a4, // task->itk_self
+    0x000001a8, // task->itk_space
+    0x000001f0, // TASK_BSD_INFO
+    0x00000090, // p->p_fd
+    0x00000000, // FILEDESC_FD_OFILES
+    0x00000008, // FILEPROC_F_FGLOB
+    0x00000028, // FILEGLOB_FG_DATA
+    0x00000010, // KSTRUCT_OFFSET_PIPE_BUFFER
 };
 static addr_t koffsets_S5L894xX_12H321[] =
 {
-    0x802D5A1D, // OSSerializer::serialize
-    0x802D7AFD, // OSSymbol::getMetaClass
-    0x8001E0A1, // calend_gettime
-    0x800C4719, // _bufattr_cpx
-    0x803ADCDC, // clock_ops
-    0x800B2744, // _copyin
-    0x800C471A, // BX LR
-    0x803F4128, // vm_kernel_addrperm
-    0x41414141, // current_task
-    0x42424242, // ipc_port_make_send
-    0x43434343, // ipc_port_copyout_send
-    0x44444444, // kernel_task
-    0x80398D08, // vtab
-    0x44,       // IPC_PORT_IP_KOBJECT
-    0xa4,       // task->itk_self
-    0x1a8,      // task->itk_space
-    0x1f0,      // TASK_BSD_INFO
-    0x90,       // p->p_fd
-    0x0,        // FILEDESC_FD_OFILES
-    0x8,        // FILEPROC_F_FGLOB
-    0x28,       // FILEGLOB_FG_DATA
-    0x10,       // KSTRUCT_OFFSET_PIPE_BUFFER
+    0x802d5a1d, // OSSerializer::serialize
+    0x802d7afd, // OSSymbol::getMetaClass
+    0x8001e0a1, // calend_gettime
+    0x800c4719, // _bufattr_cpx
+    0x803adcdc, // clock_ops
+    0x800b2744, // _copyin
+    0x800c471a, // BX LR
+    0x803f4128, // vm_kernel_addrperm
+    0x80039f2d, // current_task
+    0x80016079, // ipc_port_make_send
+    0x80016101, // ipc_port_copyout_send
+    0x80405098, // kernel_task
+    0x80398d08, // vtab
+    0x00000044, // IPC_PORT_IP_KOBJECT
+    0x000000a4, // task->itk_self
+    0x000001a8, // task->itk_space
+    0x000001f0, // TASK_BSD_INFO
+    0x00000090, // p->p_fd
+    0x00000000, // FILEDESC_FD_OFILES
+    0x00000008, // FILEPROC_F_FGLOB
+    0x00000028, // FILEGLOB_FG_DATA
+    0x00000010, // KSTRUCT_OFFSET_PIPE_BUFFER
 };
 
 static bool offsets_initialize(void)
